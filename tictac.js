958aaaa229096033b7acab9ac64d7025f4e1e7b5
Prabhat Solanki <155136172+prabhats-github@users.noreply.github.com>
2024-04-01 12:20:44 +0530

    Add files via upload

---
+++ tictac.js
@@ -0,0 +1,58 @@
+let sign='X'
+function game(id){
+    console.log(`Box is click ${id}`)
+
+    if(document.getElementById(id).innerText==''){
+        document.getElementById(id).innerText=sign
+            calResult(sign);
+        if(sign=='X'){
+            sign='O'
+        }
+        else{
+            sign='X'
+        }
+    }
+} 
+
+function calResult(sign){
+    if(document.getElementById('box1').
+    innerText==sign && document.getElementById('box2').
+    innerText==sign && document.getElementById('box3').
+    innerText==sign || document.getElementById('box4').
+    innerText==sign && document.getElementById('box5').
+    innerText==sign && document.getElementById('box6').
+    innerText==sign || document.getElementById('box7').
+    innerText==sign && document.getElementById('box8').
+    innerText==sign && document.getElementById('box9').
+    innerText==sign || document.getElementById('box1').
+    innerText==sign && document.getElementById('box4').
+    innerText==sign && document.getElementById('box7').
+    innerText==sign || document.getElementById('box2').
+    innerText==sign && document.getElementById('box5').
+    innerText==sign && document.getElementById('box8').
+    innerText==sign || document.getElementById('box3').
+    innerText==sign && document.getElementById('box6').
+    innerText==sign && document.getElementById('box9').
+    innerText==sign || document.getElementById('box1').
+    innerText==sign && document.getElementById('box5').
+    innerText==sign && document.getElementById('box9').
+    innerText==sign || document.getElementById('box3').
+    innerText==sign && document.getElementById('box5').
+    innerText==sign && document.getElementById('box7').
+    innerText==sign){
+        alert(`${sign} is WINNER!`);
+        location.reload();
+    }
+
+    
+
+
+
+
+
+
+
+
+
+    
+}
